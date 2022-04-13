8762c14a20252d7dce454e11bb8b65fc94439159
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-04-13 09:41:22 -0300

    Tipagem com TS

---
+++ reactjs/01-github-explorer/src/typeUser.ts
@@ -0,0 +1,21 @@
+type User = {
+	name: string
+	email: string
+	address: {
+		city: string
+		state?: string // ? informa que a prop. não é obrigatória
+	}
+}
+
+function showWelcomeMessage(user: User){
+	return `Welcome ${user.name}`
+}
+
+showWelcomeMessage({
+	name: 'andressa',
+	email: 'andressa@gmail',
+	address: {
+		city: 'Ipueiras',
+		state: 'CE'
+	}
+})
