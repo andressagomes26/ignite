aa66203ccf304be588cf5a3d54f88d3b3c8a0949
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-04-16 20:21:38 -0300

    Criando o modal utilizando react-modal

--- reactjs/02-dtmoney/src/App.tsx
+++ reactjs/02-dtmoney/src/App.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false);
+  }
+  
   return (
-    <div className="App">
-      <Header />
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+        <h2>Nova transação</h2>
+      </Modal>
       <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
--- reactjs/02-dtmoney/src/components/Header/index.tsx
+++ reactjs/02-dtmoney/src/components/Header/index.tsx
@@ -1,16 +1,18 @@
 import logoImg from '../../assets/logo.svg';
-
 import { Container, Content } from "./styles";
 
-export function Header(){
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+export function Header({onOpenNewTransactionModal}: HeaderProps){
     return(
-        <>
-            <Container>
-                <Content>
-                    <img src={logoImg} alt="Logo DT money" />
-                    <button type='button'>Nova Transação</button>
-                </Content>
-            </Container>
-        </>
+        <Container>
+            <Content>
+                <img src={logoImg} alt="Logo DT money" />
+                <button type='button' onClick={onOpenNewTransactionModal}>
+                    Nova Transação
+                </button>
+            </Content>
+        </Container>
     )
 }
