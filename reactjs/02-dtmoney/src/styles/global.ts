480d72c0081c45e3372c0507b60b50a942058565
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-04-18 09:37:34 -0300

    Definindo a estrutura do modal

--- reactjs/02-dtmoney/src/styles/global.ts
+++ reactjs/02-dtmoney/src/styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle `
+export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f8f2f5;
         --red: #E52E4D;
@@ -51,5 +51,28 @@ export const GlobalStyle = createGlobalStyle `
     [display]{
         display: 0.6;
         cursor: not-allowed;
+    }
+
+    .react-modal-overlay{
+        background-color: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        background-color: var(--background);
+        width: 100%;
+        max-width: 576px;
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
     }
 `
