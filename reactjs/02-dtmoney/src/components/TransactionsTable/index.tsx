424ec674600dd84667bff91d3666c56d0b0cb385
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-04-19 11:04:33 -0300

    Listando as transações com seeds e formatando datas e moedas

--- reactjs/02-dtmoney/src/components/TransactionsTable/index.tsx
+++ reactjs/02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import { Container } from "./styles";
-import { useEffect } from "react";
 import { api } from "../../services/api";
 
+interface Transactions {
+    id: number,
+    title: string,
+    type: string,
+    category: string,
+    amount: number,
+    createdAt: string
+}
 export function TransactionsTable(){
+    const [transactions, setTransactions] = useState<Transactions[]>([]);
 
     useEffect(() => {
-        api.get('transactions').then(response => console.log(response.data))
-    })
+        api.get('transactions')
+            .then(response => setTransactions(response.data.transactions))
+    }, [])
 
     return (
         <Container>
@@ -20,33 +30,21 @@ export function TransactionsTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de Sites</td>
-                        <td className="deposit">R$ 12.000,00</td>
-                        <td>Venda</td>
-                        <td>10/04/2021</td>
-                    </tr>
-
-                    <tr>
-                        <td>Hamburguer</td>
-                        <td className="withdraw">- R$ 59,00</td>
-                        <td>Alimentação</td>
-                        <td>10/04/2021</td>
-                    </tr>
-
-                    <tr>
-                        <td>Aluguel do apartamento</td>
-                        <td className="withdraw">- R$ 1.200,00</td>
-                        <td>Casa</td>
-                        <td>27/03/2021</td>
-                    </tr>
-
-                    <tr>
-                        <td>Computador</td>
-                        <td className="deposit">R$ 5.400,00</td>
-                        <td>Venda</td>
-                        <td>15/04/2021</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>{new Intl.DateTimeFormat('pt-BR').format(
+                                new Date(transaction.createdAt))}
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
