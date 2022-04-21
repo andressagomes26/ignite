20c9cfc787822a9471111dac9028f8e619bc3594
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-04-21 09:09:05 -0300

    Carregando transações

--- reactjs/02-dtmoney/src/components/TransactionsTable/index.tsx
+++ reactjs/02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
-import { api } from "../../services/api";
 
-interface Transactions {
-    id: number,
-    title: string,
-    type: string,
-    category: string,
-    amount: number,
-    createdAt: string
-}
+
+
 export function TransactionsTable(){
-    const [transactions, setTransactions] = useState<Transactions[]>([]);
 
-    useEffect(() => {
-        api.get('transactions')
-            .then(response => setTransactions(response.data.transactions))
-    }, [])
-
+    const transactions = useContext(TransactionsContext);
+    
     return (
         <Container>
             <table>
