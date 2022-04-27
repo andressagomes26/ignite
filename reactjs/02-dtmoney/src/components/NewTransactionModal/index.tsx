b5aff0c3f0d6a245ae0781384d753605ed9a7475
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-04-27 10:25:32 -0300

    Criando o hook useTransactions

--- reactjs/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import Modal from 'react-modal';
-import { FormEvent, useContext, useState } from 'react';
+import { FormEvent, useState } from 'react';
 
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -20,7 +20,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     const [amount, setAmount] = useState(0);
     const [type, setType] = useState('deposit');
     const [category, setCategory] = useState('');
-    const { createTransaction } = useContext(TransactionsContext);
+    const { createTransaction } = useTransactions();
 
     async function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
