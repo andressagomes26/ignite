669d084353e78875e7a5bda0d090751f897d6d46
Andressa Gomes Moreira <andressa_gomes.12@hotmail.com>
2022-04-27 10:11:42 -0300

    Calculando os valores de entrada e saída do summary

--- reactjs/02-dtmoney/src/components/Summary/index.tsx
+++ reactjs/02-dtmoney/src/components/Summary/index.tsx
@@ -10,8 +10,22 @@ import { Container } from "./styles";
 
 export function Summary(){
     const { transactions } = useContext(TransactionsContext);
-    console.log(transactions);
     
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 'deposit'){
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;           
+        } else {
+            acc.withdraw += transaction.amount;
+            acc.total -= transaction.amount;   
+        }
+        return acc;
+    }, {
+        deposits: 0,
+        withdraw: 0,
+        total: 0
+    });
+     
     return(
         <Container>
             <div>
@@ -19,7 +33,14 @@ export function Summary(){
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', 
+                        {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }
+                    ).format(summary.deposits)}
+                </strong>
             </div>
 
             <div>
@@ -27,7 +48,14 @@ export function Summary(){
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong>R$ 1.259,00</strong>
+                <strong> - 
+                    {new Intl.NumberFormat('pt-BR', 
+                        {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }
+                    ).format(summary.withdraw)}
+                </strong>
             </div>
 
             <div className="highlight-background">
@@ -35,7 +63,14 @@ export function Summary(){
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$ 16.141,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', 
+                        {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }
+                    ).format(summary.total)}
+                </strong>
             </div>
         </Container>
     )
